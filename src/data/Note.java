1e698e69352e1d24788465e7221717638f2f0403
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-01-04 17:45:00 +0100

    Note class's code Redundancy

--- src/data/Note.java
+++ src/data/Note.java
@@ -1,20 +1,12 @@
 package data;
 
 public class Note {
-    private String data;
     private String txt;
 
     public Note(String txt) {
         setTxt(txt);
     }
 
-    private String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
 
     public String getTxt() {
         return txt;
