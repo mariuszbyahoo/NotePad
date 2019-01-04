5f1e56b3e8a115d5f33a0f5b5af197f4ef04a251
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-01-04 17:43:39 +0100

    Adding the DatedNote class.

---
+++ src/data/DatedNote.java
@@ -0,0 +1,28 @@
+package data;
+
+public class DatedNote {
+    private String data;
+    private String txt;
+
+    public DatedNote(String txt, String data) {
+        SDayString sDayString = new SDayString();
+        setTxt(txt);
+        setData(sDayString.getData());
+    }
+
+    private String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTxt() {
+        return txt;
+    }
+
+    public void setTxt(String txt) {
+        this.txt = txt;
+    }
+}
